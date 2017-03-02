2e10b88b5c7699dcbe53a2368f48f89f0761c23e
Risal <risalkalathil@gmail.com>
2017-03-03 01:51:44 +0530

    added project file

---
+++ app/src/main/java/com/kn/risal/gang/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.kn.risal.gang;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+
+            // Using handler with postDelayed called runnable run method
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 5*1000);
+    }
+}
